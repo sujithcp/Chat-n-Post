d203e7373a009d78351e3ad33fb8a3a4e57d278a
Sujith C P <sujithp@qburst.com>
2017-09-28 13:03:19 +0530

    beautified

--- app/socket/index.js
+++ app/socket/index.js
@@ -3,99 +3,108 @@ var clients = []
 var sharedSession = require("express-socket.io-session");
 var UserList = require('../socket/user_list')
 
-function broadcast(event_type, cause){
-	Object.keys(clients).forEach(function(element) {
-		if(element==cause)
-			return
-		clients[element].emit(event_type)
-	}, this);
+function broadcast(event_type, cause, obj) {
+    Object.keys(clients).forEach(function(element) {
+        if (element == cause)
+            return
+        clients[element].emit(event_type, obj)
+    }, this);
 }
 
-function bind(a_user_email,b_user_email)
-{
-	var a = clients[a_user_email]
-	var b = clients[b_user_email]
-	a.on('disconnect', function(){
-		b.emit('status_from_server', 'Disconnected')
-		clients[a_user_email] = null
-		delete clients[a_user_email]
-	})
-	b.on('disconnect', function(){
-		a.emit('status_from_server', 'Disconnected')
-		clients[b_user_email] = null
-		delete clients[b_user_email]
-	})
-	console.log(a_user_email + " - " + b_user_email);
-	a.emit('server_message_from_server','Connected to chat mate');
-	b.emit('server_message_from_server','Connected to chat mate');
-	a.emit('ready_from_server', b_user_email)
-	b.emit('ready_from_server', a_user_email)
-	a.removeAllListeners('chat_message')
-	b.removeAllListeners('chat_message')
-	a.on('chat_message', function(data){
-		console.log("CHAT MESSAGE " + a_user_email)
-		a.emit('feedback_from_server', data)
-		b.emit('chat_message_from_server',data);
-	})
-	b.on('chat_message', function(data){
-		console.log("CHAT MESSAGE " + b_user_email)
-		b.emit('feedback_from_server', data)
-		a.emit('chat_message_from_server',data);
-	})
+function bind(a_user_email, b_user_email) {
+    var a = clients[a_user_email]
+    var b = clients[b_user_email]
+    a.on('disconnect', function() {
+        b.emit('status_from_server', 'Disconnected')
+        clients[a_user_email] = null
+        delete clients[a_user_email]
+    })
+    b.on('disconnect', function() {
+        a.emit('status_from_server', 'Disconnected')
+        clients[b_user_email] = null
+        delete clients[b_user_email]
+    })
+    console.log(a_user_email + " - " + b_user_email);
+    a.emit('server_message_from_server', 'Connected to chat mate');
+    b.emit('server_message_from_server', 'Connected to chat mate');
+    a.emit('ready_from_server', b_user_email)
+    b.emit('ready_from_server', a_user_email)
+    a.removeAllListeners('chat_message')
+    b.removeAllListeners('chat_message')
+    a.on('chat_message', function(data) {
+        console.log("CHAT MESSAGE " + a_user_email)
+        a.emit('feedback_from_server', data)
+        b.emit('chat_message_from_server', data);
+    })
+    b.on('chat_message', function(data) {
+        console.log("CHAT MESSAGE " + b_user_email)
+        b.emit('feedback_from_server', data)
+        a.emit('chat_message_from_server', data);
+    })
 }
-var init = function(server, session){
-	io = require('socket.io')(server)
-	io.use(sharedSession(session, {
-		autosave:true
-	}))
-	prepareIo()
-	return this
+var init = function(server, session) {
+    io = require('socket.io')(server)
+    io.use(sharedSession(session, {
+        autosave: true
+    }))
+    prepareIo()
+    return this
 }
-setInterval(()=>{
-	console.log(Object.keys(clients).length)
+setInterval(() => {
+    console.log(Object.keys(clients).length)
 }, 5000)
-var prepareIo = function(){
-	io.on('connection', function(connection){
-		var session = connection.handshake.session
-		var user_email = session.user_email
-		console.log("user " + user_email)
-		if(!user_email){
-			connection.disconnect()
-			return
-		}
-		if(clients[user_email]){
-			clients[user_email].disconnect()
-		}
-		connection.on('disconnect', function(){
-			console.log("Connection end : " + user_email)
-			delete clients[user_email]
-			UserList.setUserList(Object.keys(clients))
-			broadcast('user_list_update', user_email)
-			connection.removeAllListeners('chat_message')
-			connection.removeAllListeners('chat_message_from_server')
-		})
-		connection.on('connection_request', function(user){
-			if(!clients[user]){
-				connection.emit('status_from_server','Requested client not online!')
-			}
-			else{
-				clients[user].emit('connection_request_from_server', user_email)
-			}
-		})
-		connection.on('rejected', function(user){
-			clients[user].emit('rejected_from_server', user_email)
-		})
-		connection.on('ready', function(user){
-			console.log("session-name : " + user_email)
-			clients[user].emit('ready_from_server', user_email)
-			bind(user_email, user)
-			
-		})
-		connection.emit('identity_from_server', user_email)
-		console.log("New connection from : " + user_email)
-		clients[user_email] = connection
-		UserList.setUserList(Object.keys(clients))
-		broadcast('user_list_update', user_email)
-	})
+var prepareIo = function() {
+    io.on('connection', function(connection) {
+        var session = connection.handshake.session
+        var user_email = session.user_email
+        console.log("user " + user_email)
+        if (!user_email) {
+            connection.disconnect()
+            return
+        }
+        if (clients[user_email]) {
+            clients[user_email].disconnect()
+        }
+        connection.on('group_message', function(msg) {
+            broadcast('group_message_from_server', user_email, {
+                'msg': msg,
+                'sender': user_email
+            })
+        })
+
+        connection.on('disconnect', function() {
+            console.log("Connection end : " + user_email)
+            delete clients[user_email]
+            UserList.setUserList(Object.keys(clients))
+            broadcast('user_list_update', user_email)
+            broadcast("group_special_from_server", user_email, user_email + " left.")
+            connection.removeAllListeners('chat_message')
+            connection.removeAllListeners('chat_message_from_server')
+
+        })
+        connection.on('connection_request', function(user) {
+            if (!clients[user]) {
+                connection.emit('status_from_server', 'Requested client not online!')
+            } else {
+                clients[user].emit('connection_request_from_server', user_email)
+            }
+        })
+        connection.on('rejected', function(user) {
+            clients[user].emit('rejected_from_server', user_email)
+        })
+        connection.on('ready', function(user) {
+            console.log("session-name : " + user_email)
+            clients[user].emit('ready_from_server', user_email)
+            bind(user_email, user)
+
+        })
+        connection.emit('identity_from_server', user_email)
+        console.log("New connection from : " + user_email)
+        clients[user_email] = connection
+        UserList.setUserList(Object.keys(clients))
+        broadcast('user_list_update', user_email)
+        connection.emit('group_special_from_server', 'You joined now.')
+        broadcast('group_special_from_server', user_email, user_email + " joined.")
+    })
 }
 module.exports = init
--- public/js/chat_client.js
+++ public/js/chat_client.js
@@ -1,10 +1,11 @@
 var socket = io();
 current_chat = {
-	partner : '',
-	ready : false
+    partner: '',
+    ready: false
 }
 var connected = false
 var container = document.getElementById('chat')
+var groupContainer = document.getElementById('chat_group')
 var chat_msg = document.getElementById('msg')
 var button = document.getElementById('send_button')
 var me = ""
@@ -13,119 +14,174 @@ var chatUserList = document.getElementById('chat_user_list')
 var chatDiv = document.getElementById('chat_div')
 var toggle = document.getElementById('chat_header')
 var toggleIcon = document.getElementById('toggle_icon')
-toggleIcon.onclick = function(e){
-	if(chatArea.hidden){
-		chatArea.hidden = false
-		chatUserList.hidden = true
-		toggleIcon.setAttribute('src',"/assets/toggle-right.png")
-		fetchUserList()	
-	}
-	else{
-		chatArea.hidden = true
-		chatUserList.hidden = false
-		toggleIcon.setAttribute('src',"/assets/toggle-left.png")
-	}
+var singleChatDiv = document.getElementById('one_one_chat')
+var groupChatDiv = document.getElementById('group_chat')
+var groupChatButton = document.getElementById("group_chat_button")
+var groupButton = document.getElementById('send_button_group')
+var groupChatMsg = document.getElementById('msg_group')
+toggleIcon.onclick = function(e) {
+    if (chatArea.hidden) {
+        chatArea.hidden = false
+        chatUserList.hidden = true
+        toggleIcon.setAttribute('src', "/assets/toggle-right.png")
+        fetchUserList()
+    } else {
+        chatArea.hidden = true
+        chatUserList.hidden = false
+        toggleIcon.setAttribute('src', "/assets/toggle-left.png")
+    }
 }
-function createMessageNode(msg, className){
-	var item = document.createElement('div')
-	var p = document.createElement('p')
-	item.className = className
-	item.appendChild(p)
-	p.appendChild(document.createTextNode(msg))
-	return item;
+
+function createMessageNode(msg, className) {
+    var item = document.createElement('div')
+    var p = document.createElement('p')
+    item.className = className
+    item.appendChild(p)
+    p.appendChild(document.createTextNode(msg))
+    return item;
 }
-function sendRequest(e){
-	if(chatArea.hidden){
-		toggleIcon.click()
-	}
-	var div = e.target
-	console.log(e.target)
-	socket.emit('connection_request',div.innerHTML)
-	document.getElementById('status').innerHTML = "Requesting connection to " + div.innerHTML;
+
+function sendRequest(e) {
+    if (chatArea.hidden) {
+        toggleIcon.click()
+    }
+    var div = e.target
+    console.log(e.target)
+    socket.emit('connection_request', div.innerHTML)
+    document.getElementById('status').innerHTML = "Requesting connection to " + div.innerHTML;
+    singleChatDiv.hidden = false;
+    groupChatDiv.hidden = true;
 }
-function createUserItems(list){
-	document.getElementById('chat_user_list').innerHTML = ""
-	var list = JSON.parse(list)
-	for(var i in list){
-		if(list[i]==me)
-			continue;
-		var div = document.createElement('div');
-		div.setAttribute('class', 'user')
-		var name = document.createTextNode('' + list[i])
-		div.appendChild(name)
-		div.onclick = sendRequest
-		chatUserList.appendChild(div)
-	}
+
+function createUserItems(list) {
+    document.getElementById('chat_user_list').innerHTML = ""
+    var list = JSON.parse(list)
+    for (var i in list) {
+        if (list[i] == me)
+            continue;
+        var div = document.createElement('div');
+        div.setAttribute('class', 'user')
+        var name = document.createTextNode('' + list[i])
+        div.appendChild(name)
+        div.onclick = sendRequest
+        chatUserList.appendChild(div)
+    }
 }
-button.onclick = function (e){
-	console.log(connected)
-	if(!connected || chat_msg.value.trim()=="")
-		return
-	socket.emit('chat_message', chat_msg.value)
-	chat_msg.value = ""
+
+function createGroupMessageNode(object, className) {
+    var item = document.createElement('div')
+    var headP = document.createElement('p')
+    if (object.sender != '') {
+        headP.className = 'sender'
+        headP.appendChild(document.createTextNode("Member: " + object.sender))
+        item.appendChild(headP)
+    }
+
+    var p = document.createElement('p')
+    item.className = className
+    item.appendChild(p)
+    p.appendChild(document.createTextNode(object.msg))
+    return item;
+}
+button.onclick = function(e) {
+    console.log(connected)
+    if (!connected || chat_msg.value.trim() == "")
+        return
+    socket.emit('chat_message', chat_msg.value)
+    chat_msg.value = ""
+}
+groupButton.onclick = function(e) {
+    if (groupChatMsg.value.trim() == "")
+        return
+    socket.emit('group_message', groupChatMsg.value)
+    groupContainer.insertBefore(createGroupMessageNode({
+        'msg': groupChatMsg.value,
+        'sender': ''
+    }, 'self'), groupContainer.firstChild)
+    groupChatMsg.value = ""
+}
+groupChatButton.onclick = function(e) {
+    toggleIcon.click();
+    singleChatDiv.hidden = true;
+    groupChatDiv.hidden = false;
 }
 var xhr = new XMLHttpRequest();
-function fetchUserList(){
-	xhr.open('GET', '/get_user_list', true);
-	xhr.withCredentials = true
-	xhr.send(null)
-	xhr.onreadystatechange = function (e){
-		if(xhr.readyState==4){
-			if(xhr.status==200){
-				console.log(xhr.response)
-				createUserItems(xhr.response)
-				console.log('USER_LIST_UPDATE')
-			}
-			else{
-				console.log("USER_LIST_UPDATE : ERROR")
-			}
-		}
-	};
+
+function fetchUserList() {
+    xhr.open('GET', '/get_user_list', true);
+    xhr.withCredentials = true
+    xhr.send(null)
+    xhr.onreadystatechange = function(e) {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                console.log(xhr.response)
+                createUserItems(xhr.response)
+                console.log('USER_LIST_UPDATE')
+            } else {
+                console.log("USER_LIST_UPDATE : ERROR")
+            }
+        }
+    };
 }
 /*
 setInterval(()=>{
 	fetchUserList()
 }, 6000)
 */
-socket.on('connection_request_from_server', function(user){
-	if(confirm('Connect to ' + user)){
-		current_chat.partner = user;
-		connected = true
-		socket.emit('ready', user)
-	}
-	else{
-		socket.emit('rejected', user)
-	}
+socket.on('connection_request_from_server', function(user) {
+    if (confirm('Connect to ' + user)) {
+        current_chat.partner = user;
+        connected = true
+        socket.emit('ready', user)
+    } else {
+        socket.emit('rejected', user)
+    }
 })
-socket.on('ready_from_server', function(user){
-	document.getElementById('chat').innerHTML = "";
-	connected = true
-	current_chat.partner = user;
-	document.getElementById('status').innerHTML = "Connected.... to " + user;
-	if(chatArea.hidden){
-		toggleIcon.click()
-	}
+socket.on('ready_from_server', function(user) {
+    document.getElementById('chat').innerHTML = "";
+    connected = true
+    current_chat.partner = user;
+    document.getElementById('status').innerHTML = "Connected.... to " + user;
+    if (chatArea.hidden) {
+        toggleIcon.click()
+    }
+    singleChatDiv.hidden = false;
+    groupChatDiv.hidden = true;
 })
-socket.on('chat_message_from_server', function(msg){
-	console.log(msg)
-	container.insertBefore(createMessageNode(msg, 'chatmate'), container.firstChild)
+socket.on('chat_message_from_server', function(msg) {
+    console.log(msg)
+    container.insertBefore(createMessageNode(msg, 'chatmate'), container.firstChild)
 })
 
-socket.on('status_from_server', function(msg){
-	//console.log(msg)
-	document.getElementById('status').innerHTML = msg;
+socket.on('status_from_server', function(msg) {
+    document.getElementById('status').innerHTML = msg;
 })
-socket.on('feedback_from_server', function(msg){
-	container.insertBefore(createMessageNode(msg, 'self'), container.firstChild)
+socket.on('feedback_from_server', function(msg) {
+    container.insertBefore(createMessageNode(msg, 'self'), container.firstChild)
 })
-socket.on('identity_from_server', function(email){
-	me = email;
-	console.log(me)
-	fetchUserList()
+socket.on('identity_from_server', function(email) {
+    me = email;
+    console.log(me)
+    fetchUserList()
 })
-socket.on('user_list_update', function(){
-	fetchUserList()	
+socket.on('user_list_update', function() {
+    fetchUserList()
 })
-socket.on('rejected_from_server', function(){
-	document.getElementById('status').innerHTML = "Connection rejected";
+socket.on('rejected_from_server', function() {
+    document.getElementById('status').innerHTML = "Connection rejected";
+})
+
+socket.on('group_message_from_server', function(obj) {
+    groupContainer.insertBefore(createGroupMessageNode(obj, 'chatmate'), groupContainer.firstChild)
+})
+socket.on('group_special_from_server', function(msg) {
+    var item = document.createElement('div')
+    var p = document.createElement('p')
+    item.className = 'special'
+    item.appendChild(p)
+    p.appendChild(document.createTextNode(msg))
+    groupContainer.insertBefore(item, groupContainer.firstChild)
+})
+socket.on('group_status_from_server', function(obj) {
+
 })
