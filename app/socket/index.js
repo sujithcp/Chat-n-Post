65c0a8a2022ba83e21305017abe794ce65562a14
sujithp <sujithp@qburst.com>
2017-10-16 11:09:21 +0000

    Removed commented code

--- app/socket/index.js
+++ app/socket/index.js
@@ -11,39 +11,7 @@ function broadcast(event_type, cause, obj) {
         clients[element].emit(event_type, obj)
     }, this);
 }
-/*
-function bind(a_user_email, b_user_email) {
-    var a = clients[a_user_email]
-    var b = clients[b_user_email]
-    a.on('disconnect', function() {
-        b.emit('status_from_server', 'Disconnected')
-        clients[a_user_email] = null
-        delete clients[a_user_email]
-    })
-    b.on('disconnect', function() {
-        a.emit('status_from_server', 'Disconnected')
-        clients[b_user_email] = null
-        delete clients[b_user_email]
-    })
-    console.log(a_user_email + " - " + b_user_email);
-    a.emit('server_message_from_server', 'Connected to chat mate');
-    b.emit('server_message_from_server', 'Connected to chat mate');
-    a.emit('ready_from_server', b_user_email)
-    b.emit('ready_from_server', a_user_email)
-    a.removeAllListeners('chat_message')
-    b.removeAllListeners('chat_message')
-    a.on('chat_message', function(data) {
-        console.log("CHAT MESSAGE " + a_user_email)
-        a.emit('feedback_from_server', data)
-        b.emit('chat_message_from_server', data);
-    })
-    b.on('chat_message', function(data) {
-        console.log("CHAT MESSAGE " + b_user_email)
-        b.emit('feedback_from_server', data)
-        a.emit('chat_message_from_server', data);
-    })
-}
-*/
+
 var init = function(server, session) {
     global.io = require('socket.io')(server)
     io = global.io
@@ -85,25 +53,7 @@ var prepareIo = function() {
             connection.removeAllListeners('chat_message_from_server')
 
         })
-        /*
-        connection.on('connection_request', function(user) {
-            if (!clients[user]) {
-                connection.emit('status_from_server', 'Requested client not online!')
-            } else {
-                clients[user].emit('connection_request_from_server', user_email)
-            }
-        })
-        connection.on('rejected', function(user) {
-            clients[user].emit('rejected_from_server', user_email)
-        })
         
-        connection.on('ready', function(user) {
-            console.log("session-name : " + user_email)
-            clients[user].emit('ready_from_server', user_email)
-            bind(user_email, user)
-
-        })
-        */
         connection.on('chat_message', function(data){
             console.log(data)
             connection.emit('feedback_from_server', data.message)
